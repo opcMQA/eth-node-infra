277785efdae049eda1965b32474c01f0d240798e
Crypto <crypto@Alexs-MacBook-Pro.local>
2025-01-02 16:46:41 -0500

    fix: Remove snapshot-related outputs

--- terraform/modules/storage/outputs.tf
+++ terraform/modules/storage/outputs.tf
@@ -12,13 +12,3 @@ output "disk_self_link" {
   description = "The self-link of the created disk"
   value       = google_compute_disk.ethereum_data.self_link
 }
-
-output "snapshot_schedule_id" {
-  description = "The ID of the snapshot schedule"
-  value       = google_compute_resource_policy.snapshot_schedule.id
-}
-
-output "snapshot_schedule_name" {
-  description = "The name of the snapshot schedule"
-  value       = google_compute_resource_policy.snapshot_schedule.name
-}
